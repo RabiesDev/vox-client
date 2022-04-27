4da9f770979ce618a8014d19fb10aea75b83cbdc
EnDrOLl_ <66158890+Rabies1337@users.noreply.github.com>
2022-04-28 08:06:16 +0900

    AutoClicker途中
    ｈ

--- src/main/java/dev/rabies/vox/cheats/AutoClickerCheat.java
+++ src/main/java/dev/rabies/vox/cheats/AutoClickerCheat.java
@@ -1,11 +1,51 @@
 package dev.rabies.vox.cheats;
 
 import dev.rabies.vox.cheats.setting.KeyBind;
+import dev.rabies.vox.events.UpdateEvent;
+import dev.rabies.vox.utils.TimerUtil;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.commons.lang3.RandomUtils;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class AutoClickerCheat extends Cheat {
 
+    private final TimerUtil timerUtil = new TimerUtil();
+    private float nextDelay;
+
     public AutoClickerCheat() {
-        super("AutoClicker");
+        super("AutoClicker", KeyBind.fromKey(Keyboard.KEY_R));
+    }
+
+    @SubscribeEvent
+    public void onUpdate(UpdateEvent event) {
+        // TODO: 色々出来るようにします
+        if (mc.isGamePaused()) return;
+        if (!mc.inGameHasFocus) return;
+        if (!mc.gameSettings.keyBindAttack.isKeyDown()) return;
+        
+        // TODO: TimingCheck
+        // pre attack
+        if (event.isPost()) return;
+        updateDelay();
+        
+        if (!timerUtil.delay(nextDelay)) return;
+        legitAttack();
+        timerUtil.reset();
+    }
+
+    private void legitAttack() {
+        int attackKey = mc.gameSettings.keyBindAttack.getKeyCode();
+        KeyBinding.setKeyBindState(attackKey, true);
+        KeyBinding.onTick(attackKey);
+        Mouse.poll(); // ?
+    }
+    
+    private void updateDelay() {
+        float middleCps = 6; // TODO
+        float minCps = middleCps - 2;
+        float maxCps = middleCps + 2;
+    	nextDelay = 1000.0F / RandomUtils.nextFloat(minCps, maxCps);
     }
 }
---
+++ src/main/java/dev/rabies/vox/utils/TimerUtil.java
@@ -0,0 +1,27 @@
+package dev.rabies.vox.utils;
+
+public class TimerUtil {
+
+    private long prevMS = 0L;
+
+    public boolean delay(float milliSec) {
+        return ((float) this.getIncremental((getTime() - prevMS), 50.0D) >= milliSec);
+    }
+
+    public void reset() {
+        prevMS = getTime();
+    }
+
+    public long getTime() {
+        return System.nanoTime() / 1000000L;
+    }
+
+    public long getDifference() {
+        return getTime() - prevMS;
+    }
+
+    private double getIncremental(double val, double inc) {
+        double one = 1.0D / inc;
+        return Math.round(val * one) / one;
+    }
+}
