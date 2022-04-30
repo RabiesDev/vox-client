94f04426254d1f4659fd463a7879eec25f5b6c4e
EnDrOLl_ <66158890+Rabies1337@users.noreply.github.com>
2022-05-01 00:43:49 +0900

    outta here

--- src/main/java/dev/rabies/vox/events/PacketEvent.java
+++ src/main/java/dev/rabies/vox/events/PacketEvent.java
@@ -1,12 +1,13 @@
 package dev.rabies.vox.events;
 
 import lombok.Getter;
+import lombok.Setter;
 import net.minecraft.network.Packet;
 
 public class PacketEvent extends VoxEvent {
 
-    @Getter
-    private final Packet<?> packet;
+    @Getter @Setter
+    private Packet<?> packet;
 
     public PacketEvent(VoxEventTiming timing, Packet<?> packetIn) {
         super(timing);
