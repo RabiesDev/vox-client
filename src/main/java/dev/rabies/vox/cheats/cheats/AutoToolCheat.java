479e940d655527e9c6e04125f5b22fcf067be0d6
EnDrOLl_ <66158890+Rabies1337@users.noreply.github.com>
2022-05-02 03:56:38 +0900

    すいっちばっく

--- src/main/java/dev/rabies/vox/cheats/cheats/AutoToolCheat.java
+++ src/main/java/dev/rabies/vox/cheats/cheats/AutoToolCheat.java
@@ -3,6 +3,7 @@ package dev.rabies.vox.cheats.cheats;
 import dev.rabies.vox.VoxMod;
 import dev.rabies.vox.cheats.Category;
 import dev.rabies.vox.cheats.Cheat;
+import dev.rabies.vox.cheats.setting.BoolSetting;
 import dev.rabies.vox.events.UpdateEvent;
 import dev.rabies.vox.utils.ChatUtils;
 import net.minecraft.block.state.IBlockState;
@@ -12,6 +13,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class AutoToolCheat extends Cheat {
 
+    private final BoolSetting switchBackSetting = registerBoolSetting("Switch back", true);
+    private int prevSlot = -1;
+
     public AutoToolCheat() {
         super("Auto Tool", Category.RAGE);
     }
@@ -20,25 +24,33 @@ public class AutoToolCheat extends Cheat {
     public void onUpdate(UpdateEvent event) {
         if (mc.currentScreen != null) return;
         if (mc.objectMouseOver == null) return;
-        if (!mc.gameSettings.keyBindAttack.isKeyDown()) return;
-        if (mc.objectMouseOver.typeOfHit != RayTraceResult.Type.BLOCK) return;
-        IBlockState block = mc.world.getBlockState(mc.objectMouseOver.getBlockPos());
-        float strength = 1.0F;
-        int bestToolSlot = -1;
-        for (int i = 36; i < 45; i++) {
-            ItemStack itemStack = mc.player.inventoryContainer.getSlot(i).getStack();
-            if (itemStack.getDestroySpeed(block) > strength) {
-                strength = itemStack.getDestroySpeed(block);
-                bestToolSlot = i - 36;
+        if (mc.gameSettings.keyBindAttack.isKeyDown()) {
+            if (mc.objectMouseOver.typeOfHit != RayTraceResult.Type.BLOCK) return;
+            IBlockState block = mc.world.getBlockState(mc.objectMouseOver.getBlockPos());
+            float strength = 1.0F;
+            int bestToolSlot = -1;
+            for (int i = 36; i < 45; i++) {
+                ItemStack itemStack = mc.player.inventoryContainer.getSlot(i).getStack();
+                if (itemStack.getDestroySpeed(block) > strength) {
+                    strength = itemStack.getDestroySpeed(block);
+                    bestToolSlot = i - 36;
+                }
             }
-        }
 
-        if (bestToolSlot != -1) {
-            mc.player.inventory.currentItem = bestToolSlot;
-            mc.playerController.updateController();
-            if (VoxMod.get().isDebugMode()) {
-                ChatUtils.info("Switched to " + mc.player.inventory.getCurrentItem().getDisplayName());
+            if (bestToolSlot != -1) {
+                if (bestToolSlot != prevSlot) {
+                    prevSlot = mc.player.inventory.currentItem;
+                }
+
+                mc.player.inventory.currentItem = bestToolSlot;
+                mc.playerController.updateController();
+                if (VoxMod.get().isDebugMode()) {
+                    ChatUtils.info("Switched to " + mc.player.inventory.getCurrentItem().getDisplayName());
+                }
             }
+        } else if (prevSlot != mc.player.inventory.currentItem) {
+            if (!switchBackSetting.getValue()) return;
+            mc.player.inventory.currentItem = prevSlot;
         }
     }
 }
