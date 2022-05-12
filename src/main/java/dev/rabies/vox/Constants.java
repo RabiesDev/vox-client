730f12624ae6cc173a767a72bf04e09ea6573c3e
EnDrOLl_ <66158890+Rabies1337@users.noreply.github.com>
2022-05-13 01:22:59 +0900

    Update Constants.java

--- src/main/java/dev/rabies/vox/Constants.java
+++ src/main/java/dev/rabies/vox/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String MOD_NAME = "Vox";
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 
     public static final String AUTHOR = "Rabies";
 
